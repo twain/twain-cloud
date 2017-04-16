1cc8e598abdbaea9b25722e461903cf364beb3e5
Dmitry Kolomiets <kolomiets@gmail.com>
2017-04-16 15:52:07 +0300

    Add DynamoDB support to save information about sessions.

--- api-local/createSession.js
+++ api-local/createSession.js
@@ -1,26 +1,45 @@
 'use strict';
 
+const uuid = require('uuid');
+const aws = require('aws-sdk');
+const doc = require('dynamodb-doc');
+
+const awsRegion = process.env.ATALA_REGION;
+const sessionsTable = process.env.ATALA_SESSIONS_TABLE;
+
+aws.config.update({region: awsRegion});
+const dynamo = new doc.DynamoDB();
+
 module.exports.handler = (event, context, callback) => {
 
   const body = event.body;
 
-  const response = {
-    'kind': 'twainlocalscanner',
-    'commandId': body.commandId,
-    'method': 'createSession',
-    'results': {
-      'success': true,
-      'session': {
-        'sessionId': 'Session ID created by scanner for this session',
-        'revision': 1,
-        'state': 'ready',
-        'imageBlocks': []
-      }
-    }
+  const session = {
+    'sessionId': uuid.v4(), // TODO: technically, scanner should generate this
+    'revision': 1,
+    'state': 'ready',
+    'imageBlocks': []
   };
 
-  callback(null, response);
+  const params = {
+    TableName: sessionsTable,
+    Item: session
+  };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  dynamo.putItem(params, (err, data) => {
+    console.log(err);
+    console.log(data);
+
+    const response = {
+      'kind': 'twainlocalscanner',
+      'commandId': body.commandId,
+      'method': 'createSession',
+      'results': {
+        'success': true,
+        'session': session
+      }
+    };
+
+    callback(null, response);
+  });
 };
