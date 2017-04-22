780759d1dcbd9a5972eaaa6f346d4f70dd54aa71
Dmitry Kolomiets <kolomiets@gmail.com>
2017-04-22 12:59:47 +0300

    Add scanner registration API

--- api-cloud/scanners.js
+++ api-cloud/scanners.js
@@ -1,14 +1,60 @@
 'use strict';
 
-const uuid = require('uuid');
+const aws = require('aws-sdk');
+const doc = require('dynamodb-doc');
 
-module.exports.handler = (event, context, callback) => {
+const awsRegion = process.env.ATALA_REGION;
+const scannersTable = process.env.ATALA_SCANNERS_TABLE;
 
-  callback(null, [
-    uuid.v4(),
-    uuid.v4()
-  ]);
+aws.config.update({region: awsRegion});
+const dynamo = new doc.DynamoDB();
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.getScanners = (event, context, callback) => {
+
+  const params = {
+    TableName: scannersTable
+  };
+
+  let scanners = [];
+
+  dynamo.scan(params, function onScan(err, data) {
+    if (err) return callback(err);
+
+    data.Items.forEach(function(scanner) {
+      scanner.url = `/scanners/${scanner.id}`;
+      scanner.api = [
+        `/scanners/${scanner.id}/privet/info`,
+        `/scanners/${scanner.id}/privet/twaindirect/session`
+      ];
+    });
+
+    scanners = scanners.concat(data.Items);
+
+    // continue scanning if we have more movies, because
+    // scan can retrieve a maximum of 1MB of data
+    if (typeof data.LastEvaluatedKey != 'undefined') {
+      console.log('Scanning for more...');
+      params.ExclusiveStartKey = data.LastEvaluatedKey;
+      dynamo.scan(params, onScan);
+    } else {
+      // return found scanners to the client
+      callback(null, scanners);
+    }
+  });
+};
+
+module.exports.loginScanner = (event, context, callback) => {
+
+  const scannerInfo = event.body;
+  console.log(scannerInfo);
+
+  const params = {
+    TableName: scannersTable,
+    Item: scannerInfo
+  };
+
+  dynamo.putItem(params, (err, data) => {
+    if (err) return callback(err);
+    callback(null, data);
+  });
 };
