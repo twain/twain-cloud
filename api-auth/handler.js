0cab35e3b5586915b0f10e70c6a0013268d32cb7
Dmitry Kolomiets <kolomiets@gmail.com>
2018-01-18 10:43:28 +0300

    Port auth lambdas from boilerplate code

---
+++ api-auth/handler.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const signinHandler = require('./handlers/signinHandler');
+const callbackHandler = require('./handlers/callbackHandler');
+const refreshHandler = require('./handlers/refreshHandler');
+const authorizeHandler = require('./handlers/authorizeHandler');
+
+module.exports.signin =
+  (event, context) =>
+    signinHandler(event, context);
+
+module.exports.callback =
+  (event, context) =>
+    callbackHandler(event, context);
+
+module.exports.refresh =
+  (event, context, cb) =>
+    refreshHandler(event, cb);
+
+module.exports.authorize =
+  (event, context, cb) =>
+    authorizeHandler(event, cb);
+
---
+++ api-auth/helpers.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const createResponseData = (id) => {
+  // sets 15 seconds expiration time as an example
+  const authorizationToken = {
+    payload: {
+      id
+    },
+    options: {
+      expiresIn: 15
+    }
+  };
+
+  return { authorizationToken };
+};
+
+const redirectProxyCallback = (context, data) => {
+  context.succeed({
+    statusCode: 302,
+    headers: {
+      Location: data.url
+    }
+  });
+};
+
+const log = (message) => {
+  console.debug(message);
+};
+
+exports = module.exports = {
+  createResponseData,
+  redirectProxyCallback,
+  log
+};
