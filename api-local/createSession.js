782c96e5fd437158446e45722a58b03b20d02fd3
Dmitry Kolomiets <kolomiets@gmail.com>
2018-01-22 11:46:33 +0300

    Update createSession to generate MQTT signed URL

--- api-local/createSession.js
+++ api-local/createSession.js
@@ -9,37 +9,59 @@ const sessionsTable = process.env.TWAIN_SESSIONS_TABLE;
 module.exports.handler = (event, context, callback) => {
 
   const body = event.body;
+  const clientId = event.principalId;
   const scannerId = event.path.scannerId;
   const sessionId = uuid.v4(); // TODO: technically, scanner should generate this
-
   // TODO: add scanner id validation!
 
-  const session = {
-    'sessionId': sessionId,
-    'revision': 1,
-    'state': 'ready',
-    'imageBlocks': [],
-    '_scannerId': scannerId
-  };
-
-  const params = {
-    TableName: sessionsTable,
-    Item: session
-  };
-
-  db.putItem(params).promise().then(() => {
-    return iot.notifyScanner(scannerId, { command: 'createSession', parameters: { sessionId: sessionId } });
-  }).then(() => {
-    const response = {
-      'kind': 'twainlocalscanner',
-      'commandId': body.commandId,
-      'method': 'createSession',
-      'results': {
-        'success': true,
-        'session': session
-      }
+  // generate signed MQTT Url
+  iot.signMqttUrl(context)
+  // create session object 
+  .then(iotUrl => {
+    const session = {
+      sessionId: sessionId,
+      revision: 1,
+      state: 'ready',
+      imageBlocks: [],
+      eventSource: {
+        type: 'mqtt',
+        url: iotUrl,
+        topic: iot.getClientTopic(sessionId)
+      },
+      _scannerId: scannerId,
+      _clientId: clientId
     };
 
-    callback(null, response);
-  }).catch(callback);
+    return session;
+  })
+  .then(session => {
+    const params = {
+      TableName: sessionsTable,
+      Item: session
+    };
+    
+    return db.putItem(params).promise()
+    .then(() => {
+      return iot.notifyScanner(scannerId, { 
+        command: 'createSession', 
+        parameters: { 
+          sessionId: sessionId 
+        } 
+      });
+    })
+    .then(() => {
+      const response = {
+        'kind': 'twainlocalscanner',
+        'commandId': body.commandId,
+        'method': 'createSession',
+        'results': {
+          'success': true,
+          'session': session
+        }
+      };
+  
+      callback(null, response);
+    });
+  })
+  .catch(callback);
 };
