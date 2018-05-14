1687913f9867b25be45ca729e7199b7e55cb260f
Dmitry Kolomiets <kolomiets@gmail.com>
2018-05-14 07:39:51 +0300

    Get rid of console.log statements

--- api-cloud/download.js
+++ api-cloud/download.js
@@ -2,15 +2,16 @@
 
 const AWS = require('../aws');
 const s3 = new AWS.S3({ signatureVersion: 'v4' });
+const { apiGatewayHandler } = require('../utils/lambda');
 
 const bucket = process.env.TWAIN_BUCKET;
 
-module.exports.handler = (event, context, callback) => {
+module.exports.handler = apiGatewayHandler((event, context, callback, env) => {
   const scannerId = event.path.scannerId;
   const blockId = event.path.blockId;
   const fileId = `${scannerId}/${blockId}`;
 
-  console.log('downloading file');
+  env.logger.info('downloading file');
   const downloadOptions = { Bucket: bucket, Key: fileId };
   s3.getObject(downloadOptions).promise()
   .then(blob => {
@@ -18,4 +19,4 @@ module.exports.handler = (event, context, callback) => {
     callback(null, base64);
   })
   .catch(callback);
-};
+});
--- api-cloud/upload.js
+++ api-cloud/upload.js
@@ -3,20 +3,21 @@
 const uuid = require('uuid');
 const AWS = require('../aws');
 const s3 = new AWS.S3({ signatureVersion: 'v4' });
+const { apiGatewayHandler } = require('../utils/lambda');
 
 const bucket = process.env.TWAIN_BUCKET;
 
-module.exports.handler = (event, context, callback) => {
+module.exports.handler = apiGatewayHandler((event, context, callback, env) => {
   const scannerId = event.path.scannerId;
   const blockId = uuid.v4();
 
   const fileId = `${scannerId}/${blockId}`;
   const uploadOptions = { Bucket: bucket, Key: fileId, Body: new Buffer(event.body, 'base64') };
 
-  console.log('saving file');
+  env.logger.info('saving file');
   s3.putObject(uploadOptions).promise()
   .then(() => {
     callback(null, blockId);
   })
   .catch(callback);
-};
+});
