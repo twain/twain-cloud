f550062094da10cd73fc1e58098bf1c12bf67303
Dmitry Kolomiets <kolomiets@gmail.com>
2018-05-14 06:47:56 +0300

    Cleanup of upload lambda

--- api-cloud/upload.js
+++ api-cloud/upload.js
@@ -7,14 +7,16 @@ const s3 = new AWS.S3({ signatureVersion: 'v4' });
 const bucket = process.env.TWAIN_BUCKET;
 
 module.exports.handler = (event, context, callback) => {
-  const folder = event.path.scannerId;
-  const fileId = `${folder}/${uuid.v4()}`;
+  const scannerId = event.path.scannerId;
+  const blockId = uuid.v4();
+
+  const fileId = `${scannerId}/${blockId}`;
   const uploadOptions = { Bucket: bucket, Key: fileId, Body: new Buffer(event.body, 'base64') };
 
   console.log('saving file');
-
-  s3.putObject(uploadOptions, function (err) {
-    if (err) return callback(err);
-    callback(null, fileId);
-  });
+  s3.putObject(uploadOptions).promise()
+  .then(() => {
+    callback(null, blockId);
+  })
+  .catch(callback);
 };
